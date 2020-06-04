10f2d2da732ae9bd8007a28ecd747b9c071a4156
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-06-04 14:31:09 -0300

    add route get user by id

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,24 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+  'Isaac',
+  'Carlos',
+  'Joaquim'
+];
+
 app.get('/users', (request, response) => {
   console.log('Listagem de usuários');
 
-  response.json([
-    'Isaac',
-    'Carlos',
-    'Joaquim'
-  ]);
+  response.json(users);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  response.json(user);
 });
 
 app.post('/users', (request, response) => {
