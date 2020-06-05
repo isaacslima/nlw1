c7b9c9737085a05947c8b347d76a5a962f08e26f
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-06-05 20:50:20 -0300

    add file routes and import to main server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json);
 
-const users = [
-  'Isaac',
-  'Carlos',
-  'Joaquim'
-];
-
-app.get('/users', (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search ? users.filter(user => users.includes(search)) : users;
-
-  response.json(users);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  response.json(user);
-});
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-
-  const user = { 
-    name: data.name,
-    email: data.email
-  }
-
-  return response.json(user);
-});
-
+app.use(routes);
 
 app.listen(3333)
