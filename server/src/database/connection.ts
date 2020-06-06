26f8fc7d9796f27637bd8bb7266bf32322bcf8d0
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-06-06 10:44:19 -0300

    finish all migrations

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -4,7 +4,8 @@ const connection = Knex({
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite')
-    }
+    },
+    useNullAsDefault: true,
 })
 
 export default connection;
