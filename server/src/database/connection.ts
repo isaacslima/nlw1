543e061d5946c20987998d2e3c40cb21c912e69c
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-06-07 16:21:54 -0300

    add rout itens

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,5 +1,6 @@
 import Knex from 'knex';
 import path from 'path';
+
 const connection = Knex({
     client: 'sqlite3',
     connection: {
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,47 @@
 import express from 'express';
+import knex from './database/connection'
 
 const routes  = express.Router();
 
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return { 
+            title: item.name,
+            image_url: `http://localhost/3333/uploads/${item.image}`
+        };
+    })
+    return response.json(items);
 });
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        number,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        number,
+        city,
+        uf
+    });
+
+    return response.json({ success: true});
+})
 
 export default routes;
