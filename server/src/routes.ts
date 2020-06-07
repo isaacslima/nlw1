86552f61220211b327628e8e808ff766934dc8bf
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-06-07 17:38:09 -0300

    finish routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,14 +3,13 @@ import express from 'express';
 import PointsController from './controllers/PointsControllers';
 import ItemsController from './controllers/ItemsController';
 
-// index, show, create, update, delete
 const routes  = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 routes.post('/points', pointsController.create);
-
+routes.get('/points', pointsController.index)
 routes.post('/points:/id', pointsController.show);
 
 export default routes;
